7b18b40b1e3dbdfff00f0ad4e6d42e49ed3ecdb3
IgnasK-mill <ignaskrusinskas@gmail.com>
2026-07-24 23:20:18 +0300

    Update content.js

--- src/content.js
+++ src/content.js
@@ -5,7 +5,7 @@ export const content = {
     tagline: "Fotografė · Kaunas",
     instagram: "gudasiute_photography",
     instagramUrl: "https://instagram.com/gudasiute_photography",
-    email: "vaidile.gudasiute@gmail.com",
+    email: "vaidilephotography@gmail.com",
     phone: "+370 623 45 678",
     location: "Kaunas, Lietuva",
   },
